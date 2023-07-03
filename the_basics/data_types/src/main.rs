bb4292249d2be7596774d05c42d351f988bc318d
Tulio Olivieri <tuliolivieri@gmail.com>
2023-07-03 00:44:17 -0300

    feat: variables, mutability and scalar data types

---
+++ the_basics/data_types/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let x: i8 = 10;
+    println!("{}", x);
+
+    let y: u8 = 10;
+    println!("{}", y);
+
+    let decimal = 02_55;
+    let hex = 0xff;
+    let octal = 0o377;
+    let binary = 0b1111_1111;
+
+    println!("{}", decimal);
+    println!("{}", hex);
+    println!("{}", octal);
+    println!("{}", binary);
+
+    let byte = b'A';
+    println!("{}", byte);
+
+    let w = 2.341;
+    let z: f32 = 1.21312;
+
+    println!("{}", w);
+    println!("{}", z);
+
+    let t = true;
+    let f: bool = false;
+
+    println!("{}", t);
+    println!("{}", f);
+
+    let c = 'c';
+    println!("{}", c);
+
+    // + - * / %
+}
---
+++ the_basics/variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut x = 5;
+    println!("The value o x is {}", x);
+
+    x = 10;
+    println!("The value o x is {}", x);
+
+    const SECONDS: i8 = 60;
+    println!("The value o seconds is {}", SECONDS);
+}
